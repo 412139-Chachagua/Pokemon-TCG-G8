02b5e3fe2c43dcdb72d784a3be20c71b788820ce
412285-Cotaimich <412285@tecnicatura.frc.utn.edu.ar>
2026-06-29 23:00:11 -0300

    fix for auth service

--- FE/src/app/app.config.ts
+++ FE/src/app/app.config.ts
@@ -4,12 +4,13 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { AudioService } from './core/audio/audio.service';
+import { authErrorInterceptor } from './core/interceptors/auth-error.interceptor';
 import { authInterceptor } from './core/interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideHttpClient(withInterceptors([authInterceptor])),
+    provideHttpClient(withInterceptors([authInterceptor, authErrorInterceptor])),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAppInitializer(() => { inject(AudioService); })
